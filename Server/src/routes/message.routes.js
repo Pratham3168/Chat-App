d6f87af426933d13c0dc3570bfcc12f9f0c6127c
Pratham3168 <prathamtanwar593@gmail.com>
2026-03-25 01:46:13 +0530

    feat: implement message handling with CRUD operations and integrate Arcjet protection

--- Server/src/routes/message.routes.js
+++ Server/src/routes/message.routes.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+import { getAllContacts ,getChatPartners,getMessagesByUserId, sendMessage } from '../controllers/message.controller.js';
+import { arcjetProtection } from '../middlewares/arcjet.middleware.js';
 
 const router = express.Router();
 
+router.use(arcjetProtection,protectRoute); // Apply Arcjet protection and authentication middleware to all routes in this router
 
+router.get('/contacts',getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id",  getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 router.get('/send',(req,res)=>{
     res.send('Send Message Endpoint');
