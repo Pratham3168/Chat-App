6f44256ef36608746c7541dc724b5f79c9dfc8b1
Pratham3168 <prathamtanwar593@gmail.com>
2026-03-27 22:42:02 +0530

    feat: implement optimistic message sending in chat store

--- Client/src/stores/useChatStore.js
+++ Client/src/stores/useChatStore.js
@@ -1,6 +1,7 @@
 import toast from 'react-hot-toast';
 import {create} from 'zustand';
 import { axiosInstance } from '../lib/axios';
+import { useAuthStore } from './useAuthStore';
 
 export const useChatStore = create((set,get) => ({
     allContacts : [],
@@ -73,12 +74,30 @@ export const useChatStore = create((set,get) => ({
             return;
         }
 
-       try{
+        const { authUser } = useAuthStore.getState();
+        const tempId = `temp-${Date.now()}`;
+
+        const optimisticMessage = {
+            _id: tempId,
+            senderId: authUser._id,
+            receiverId: selectedUser._id,
+            text: messageData.text,
+            image: messageData.image,
+            createdAt: new Date().toISOString(),
+            isOptimistic: true,
+        };
+
+        set((state) => ({ messages: [...state.messages, optimisticMessage] }));
+
+        try {
             const res = await axiosInstance.post(`/message/send/${selectedUser._id}`, messageData);
-            set((state) => ({ messages: [...state.messages, res.data] }));
-       }catch(err){
+            set((state) => ({
+                messages: state.messages.map((msg) => (msg._id === tempId ? res.data : msg)),
+            }));
+        } catch (err) {
+            set((state) => ({ messages: state.messages.filter((msg) => msg._id !== tempId) }));
             toast.error(err.response?.data?.message || "Failed to send message");
-       }
+        }
 
     }
 
