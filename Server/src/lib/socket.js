82a5c0328b2ca567d4982ad625919165da6862d5
Pratham3168 <prathamtanwar593@gmail.com>
2026-03-28 18:11:54 +0530

    fix: import socketAuthMiddleware in socket.js

--- Server/src/lib/socket.js
+++ Server/src/lib/socket.js
@@ -1,6 +1,7 @@
 import {Server} from 'socket.io';
 import http from 'http';
 import express from 'express';
+import { socketAuthMiddleware } from '../middlewares/socket.auth.middleware.js';
 
 const app = express();
 
